cdd50efc24c9425386cd385834e84d94e5f5453b
smallnews <1371606921@qq.com>
2024-12-30 22:19:31 +0800

    框架安装部署

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
+
+    bbasdfasdfasdf
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Livewire\Repeater;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('repeater', Repeater::class);
