c9b81aac8d9c2055ad2ce72b41e2375bf7848508
smallnews <1371606921@qq.com>
2025-02-04 18:20:02 +0800

    更新 扩展包中的静态资源了

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/wsmallnews/**/*.blade.php',
+        './addons/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/**/*.blade.php',
         './resources/**/*.js',
