0b3ee8fbd252363106832f48399019d8e1ad4a0a
smallnews <1371606921@qq.com>
2025-10-31 23:09:45 +0800

    更新代码

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
+    $category = \Wsmallnews\Category\Models\CategoryType::query()
+        ->scopeable('aaa', 666)
+        ->firstOrCreate(['level' => 5]);
+
+    dd($category);
+
     return view('welcome');
 });
