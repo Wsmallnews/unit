f7f5f36df468edd082e185e6c54036201278683d
smallnews@qq.com <1371606921@qq.com>
2025-11-29 17:45:36 +0800

    tags migrate

---
+++ database/migrations/2025_11_25_102654_create_tag_tables.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+
+            // sn 新增字段
+            $table->unsignedBigInteger('team_id')->nullable()->comment('团队ID');
+            $table->string('scope_type', 60)->nullable()->comment('范围类型');
+            $table->unsignedBigInteger('scope_id')->default(0)->comment('范围');
+
+            $table->json('name');
+            $table->json('slug');
+            $table->string('type')->nullable();
+            $table->integer('order_column')->nullable();
+
+            $table->timestamps();
+        });
+
+        Schema::create('taggables', function (Blueprint $table) {
+            $table->foreignId('tag_id')->constrained()->cascadeOnDelete();
+
+            $table->morphs('taggable');
+
+            $table->unique(['tag_id', 'taggable_id', 'taggable_type']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('taggables');
+        Schema::dropIfExists('tags');
+    }
+};
